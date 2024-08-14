a9ee2cb8548def7e0b8085ac1ea439e2b7e9b5ce
bon0512 <nonstop00450@gmail.com>
2024-08-14 21:32:29 +0900

    7장 데이터 수정하기

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -9,17 +9,19 @@ import java.beans.ConstructorProperties;
 //@AllArgsConstructor //생성자 자동생성
 @ToString //toString 자동생성
 public class ArticleForm {
+    private Long id;
     private String title;
     private String content;
 
-
-
-    @ConstructorProperties({"title","content"})
-    public ArticleForm(String title, String content) {
+    @ConstructorProperties({"id", "title", "content"})
+    public ArticleForm(Long id, String title, String content) {
+        this.id = id;
         this.title = title;
         this.content = content;
     }
+
+
     public Article toEntity() {
-        return new Article(null,title,content);
+        return new Article(id,title,content);
     }
 }
