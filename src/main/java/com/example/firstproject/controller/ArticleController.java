f8eecee0db6bf1415feb831fa06c5cac9b37e79e
bon0512 <nonstop00450@gmail.com>
2024-08-15 23:01:33 +0900

    8장 완료

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Controller
 @Slf4j
@@ -109,6 +112,27 @@ public class ArticleController {
 
 
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id ,RedirectAttributes rttr){         //삭제 요청 메서드
+        log.info("삭제 요청이 들어왔습니다");
+
+        //1. 삭제할 대상 가져오기(리파지터리 이용)
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        //2. 대상 엔티티 삭제하기
+        if(target!=null){       //삭제할 대상이 있으면
+            articleRepository.delete(target);   //대상을 삭제하기
+            rttr.addFlashAttribute("msg","삭제 됬습니다!");
+            /*RedirectAttributes 객체로 리다이렉트 페이지에서 사용할 데이터를 남길 수 있다.
+            *  addFlashAttribute메서드를 사용하면 리다이렉트 시점에 한번만 사용할 수 있는 데이터를 등록가능
+            *   리다이렉트된 페이지에서 설정을해줘야한다 여기서는 index.html*/
+        }
+
+        //3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles" ;
     }
 
 }
